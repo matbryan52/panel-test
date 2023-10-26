e70465fee610394eb73ca92a6c4ae4a14d9ef893
Matthew Bryan <matthewbryan52@gmail.com>
2023-10-26 08:13:04 +0200

    add run.py

---
+++ run.py
@@ -0,0 +1,23 @@
+import panel as pn
+
+stylesheet = """
+    #divContainer input[type="file"] {
+      border-radius: 4px;
+}
+"""
+
+def make_layout():
+    inp = pn.widgets.FileInput()
+    inp.stylesheets=[stylesheet]
+
+    output = pn.widgets.StaticText(value="None")
+
+    def _print_value(e):
+        output.value = inp.filename
+
+    btn = pn.widgets.Button(name='Show')
+    btn.on_click(_print_value)
+
+    return pn.Column(inp, output, btn)
+
+make_layout().servable()
